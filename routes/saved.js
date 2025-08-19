79faea3e435f4ccb7f5ce48c6c390627e6562aeb
SanamR14 <sanamrajaboopathi@gmail.com>
2025-08-19 22:37:16 +0100

    saved api

--- routes/saved.js
+++ routes/saved.js
@@ -12,6 +12,6 @@ const {
 router.post("/", addSavedItem);
 router.get("/:userid", getUserSavedItems);
 router.get("/:userid/:item_id", getSavedItem);
-router.delete("/:userid/:item_id", deleteSavedItem);
+router.delete("/:userid/:id", deleteSavedItem);
 
 module.exports = router;
