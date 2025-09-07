99b00d4fc6e39e00c2034956fa6dd108eae0f891
SanamR14 <sanamrajaboopathi@gmail.com>
2025-09-07 23:16:59 +0100

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,13 +14,17 @@ exports.getUsers = async (req, res) => {
 };
 
 exports.getUserById = async (req, res) => {
+  const { id } = req.params;
   try {
-    const result = await pool.query("SELECT * FROM users WHERE id=$1", [id]);
+    const result = await pool.query(
+      'SELECT id, name, email, city, country FROM "users" WHERE id = $1',
+      [id]
+    );
     if (result.rows.length === 0) {
-      return res.status(200).json([]);
+      return res.status(404).json({ error: "User not found" });
     }
 
-    res.status(200).json(result.rows);
+    res.status(200).json(result.rows[0]);
   } catch (err) {
     res.status(500).json({ error: "Internal server error" });
   }
