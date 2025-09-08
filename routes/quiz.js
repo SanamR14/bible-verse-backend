94a606374c01d779fb7a22519774a2c0b14e0043
SanamR14 <sanamrajaboopathi@gmail.com>
2025-09-08 20:25:31 +0100

    fixed issue

--- routes/quiz.js
+++ routes/quiz.js
@@ -5,6 +5,6 @@ const quizController = require("../controllers/quizController");
 router.post("/", quizController.createQuiz);
 router.post("/:quiz_id/questions", quizController.addQuestion);
 router.post("/:quiz_id/session", quizController.createSession);
-router.get("/quiz/:quiz_id/questions", quizController.getQuestions);
+router.get("/:quiz_id/questions", quizController.getQuestions);
 
 module.exports = router;
