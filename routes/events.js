bb5ac56e79ad16039ed61ea76bbdda80507447c0
SanamR14 <sanamrajaboopathi@gmail.com>
2025-09-24 23:17:24 +0100

    test changes

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getEvents, createEvent } = require("../controllers/events");
+const { getEvents, createEvent } = require("../controllers/eventsController");
 
 const authMiddleware = require("../middleware/auth");
 
