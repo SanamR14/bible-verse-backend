05cdf02ffb05c6ddee0293a7af2acdf4ed656523
SanamR14 <sanamrajaboopathi@gmail.com>
2025-09-29 23:29:53 +0100

    added auth

--- routes/quiz.js
+++ routes/quiz.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const quizController = require("../controllers/quizController");
 
-router.post("/", quizController.createQuiz);
-router.post("/:quiz_id/questions", quizController.addQuestion);
-router.post("/:quiz_id/session", quizController.createSession);
-router.get("/:quiz_id/questions", quizController.getQuestions);
-router.get("/all", quizController.getAllQuizzes);
-router.delete("/delete/:quizId", quizController.deleteQuiz);
+const authMiddleware = require("../middleware/auth");
+
+router.post("/", authMiddleware, quizController.createQuiz);
+router.post("/:quiz_id/questions", authMiddleware, quizController.addQuestion);
+router.post("/:quiz_id/session", authMiddleware, quizController.createSession);
+router.get("/:quiz_id/questions", authMiddleware, quizController.getQuestions);
+router.get("/all", authMiddleware, quizController.getAllQuizzes);
+router.delete("/delete/:quizId", authMiddleware, quizController.deleteQuiz);
 
 module.exports = router;
